17b1ca6151702252561fe124c0c012b4a2a3e399
wael Ashi <waelashi1@gmail.com>
2019-05-25 16:10:39 +0300

    testGithubPage

---
+++ build/precache-manifest.653774ed4fc5d25030cbf324ec735e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d430d4a93202fa97a2d4f0dee61b004",
+    "url": "/portfoilo/index.html"
+  },
+  {
+    "revision": "dc35d87813b1308a59e5",
+    "url": "/portfoilo/static/css/main.e6e65813.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfoilo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dc35d87813b1308a59e5",
+    "url": "/portfoilo/static/js/main.9a009566.chunk.js"
+  },
+  {
+    "revision": "ae4f3f5f075a3b76cba7",
+    "url": "/portfoilo/static/js/runtime~main.7aafe786.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfoilo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Wael Achi
+          Raed  Shihab
         </a>
       </header>
     </div>
